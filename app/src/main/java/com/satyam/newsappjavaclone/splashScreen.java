5d0dcb572810871ba1149dc4a6da16badce55f48
Satyam Prajapati <satyam7355pra@gmail.com>
2021-11-08 22:04:50 +0530

    commited on 8 Nov

---
+++ app/src/main/java/com/satyam/newsappjavaclone/splashScreen.java
@@ -0,0 +1,26 @@
+package com.satyam.newsappjavaclone;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class splashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(splashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },2000);
+    }
+}
